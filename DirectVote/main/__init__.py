a545ef746b8df0c2050b8ad1c0e7aade193f4c02
Filipe Silva <filipesilva@Filipes-iMac.local>
2015-04-10 17:48:32 +0200

    added

---
+++ DirectVote/main/__init__.py
@@ -0,0 +1,2 @@
+__author__ = "filipesilva"
+__date__ = "$Apr 10, 2015 2:30:20 PM$"
---
+++ DirectVote/main/urls.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "filipesilva"
+__date__ = "$Apr 10, 2015 2:31:01 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
