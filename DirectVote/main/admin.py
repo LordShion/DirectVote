66ca4025f49a0e2406e0c1bf5bacf2c7a5b4c061
filipe silva <filipe.silva@conject.com>
2016-08-12 23:20:19 +0200

    added votations and proposal texts

--- DirectVote/main/admin.py
+++ DirectVote/main/admin.py
@@ -1,7 +1,44 @@
 from django.contrib import admin
-from DirectVote.main.models import ValidUser
+from DirectVote.main.models import ValidUser, Proposal, Votation
 
 class ValidUserAdmin(admin.ModelAdmin):
     list_display = ('user', 'idcard')
     pass
 admin.site.register(ValidUser, ValidUserAdmin)
+
+class VotationAdmin(admin.ModelAdmin):
+    list_display = (
+                        'proposal',
+                        'nb_pro_votes',
+                        'nb_con_votes',
+                        'nb_blank_votes',
+                        'total_votes',
+                        'total_available_voters',
+                        'start_date',
+                        'end_date',
+                        'created',
+                        'created_by',
+                        'modified',
+                        'modified_by',
+                        )
+    pass
+admin.site.register(Votation, VotationAdmin)
+
+#class UsersInline(admin.TabularInline):
+#    model = ValidUser
+#    extra = 1
+
+
+class ProposalAdmin(admin.ModelAdmin):
+    #inlines = (UsersInline,)
+    list_display = (
+                        'text',
+                        #'authors',
+                        'created',
+                        'created_by',
+                        'modified',
+                        'modified_by',
+                        )
+    pass
+admin.site.register(Proposal, ProposalAdmin)
+
--- DirectVote/main/models.py
+++ DirectVote/main/models.py
@@ -1,11 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
 class ValidUser(models.Model):
     user = models.OneToOneField(User)
     idcard = models.BigIntegerField(primary_key=True)
-    
+
     class Meta:
         unique_together = ('user', 'idcard',)
-    
+
+# Proposals
+class Proposal(models.Model):
+    text = models.TextField(default='Proposal text', max_length=20000)
+    authors = models.ManyToManyField(ValidUser)
+    created = models.DateTimeField(editable=False)
+    created_by = models.ForeignKey(User, related_name='Proposal_created_by', on_delete=models.CASCADE, editable=False)
+    modified = models.DateTimeField(editable=False)
+    modified_by = models.ForeignKey(User,related_name='Proposal_modified_by', on_delete=models.CASCADE, editable=False)
+
+
+# Votations
+class Votation(models.Model):
+    proposal = models.OneToOneField(Proposal, editable=False)
+    nb_pro_votes = models.IntegerField(default=0, editable=False)
+    nb_con_votes = models.IntegerField(default=0, editable=False)
+    nb_blank_votes = models.IntegerField(default=0, editable=False)
+    total_votes = models.IntegerField(default=0, editable=False)
+    total_available_voters = models.IntegerField(default=0, editable=False)
+    created = models.DateTimeField(editable=False)
+    created_by = models.ForeignKey(User, related_name='Votation_created_by', on_delete=models.CASCADE, editable=False)
+    modified = models.DateTimeField(editable=False)
+    modified_by = models.ForeignKey(User,related_name='Votation_modified_by', on_delete=models.CASCADE, editable=False)
+
+    start_date = models.DateTimeField(editable=False)
+    end_date = models.DateTimeField(editable=False)
+
+
